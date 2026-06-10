c19d3f2ff525a7031cab6352b4f43f7f781e3ad4
Vinod Guptha <Vinodhkumar0339@gmail.com>
2026-06-10 13:21:57 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Learning Devops
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
