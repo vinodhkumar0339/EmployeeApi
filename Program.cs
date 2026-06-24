f604617428729c11cc69f4cf26ef691720419e2a
Vinod Guptha <Vinodhkumar0339@gmail.com>
2026-06-24 11:23:57 +0530

    Test from Dev environment

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // Learning Devops
+//Push Code Test from Visual Studio
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
